808f1bce471e5d7710ed59cdcbe1cbbf21a2a10c
Frank Corona Prendes <frankdavid.corona@gmail.com>
2020-05-31 16:36:26 -0500

    Updated usuarios require in server.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const { conectarBaseDeDatos } = require('./bd')
 
 // rutas de componentes
-const rutasDeUsuarios = require('./componentes/usuarios/usuario.rutas')
+// const rutasDeUsuarios = require('./componentes/usuarios/usuario.rutas')
 const rutasDeProyectos = require('./componentes/proyectos/rutas')
 
 // body parser
@@ -22,11 +22,13 @@ app.get('/', function (solicitud, respuesta) {
 })
 
 // usar rutas de los componentes
-app.use('/usuarios', rutasDeUsuarios)
+// app.use('/usuarios', rutasDeUsuarios)
 app.use('/proyectos', rutasDeProyectos)
 
 // rutas para los avatares
 app.use('/avatares', express.static('avatares'))
+
+require('./componentes/usuarios/usuario.rutas')(app);
 
 // Encendemos el servidor de express
 app.listen(PUERTO, function () {
